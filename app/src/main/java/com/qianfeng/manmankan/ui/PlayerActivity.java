b64df500f2d67926ff0f5e525973152f69499ae1
wRedRose <wuyaqionline@yeah.net>
2016-09-20 15:13:15 +0800

    接口地址更新

---
+++ app/src/main/java/com/qianfeng/manmankan/ui/PlayerActivity.java
@@ -0,0 +1,15 @@
+package com.qianfeng.manmankan.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.qianfeng.manmankan.R;
+
+public class PlayerActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
