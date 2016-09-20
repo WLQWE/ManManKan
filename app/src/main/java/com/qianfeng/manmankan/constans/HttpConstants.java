5b83ad8b1585799dbc118d17210fa281a3fc339d
wRedRose <wuyaqionline@yeah.net>
2016-09-20 14:25:57 +0800

    接口地址更新

--- app/src/main/java/com/qianfeng/manmankan/constans/HttpConstants.java
+++ app/src/main/java/com/qianfeng/manmankan/constans/HttpConstants.java
@@ -9,7 +9,7 @@ public class HttpConstants {
     public static final String  RECOMMEND="http://www.quanmin.tv/json/page/appv2-index/info.json?09201152&device=869953020997409&v=2.1.3&screen=2&ch=wandoujia&sh=1812&sw=1080&uid=WTKDU16629006724&net=0&ver=4&os=1";
     public static final String PROGRAMA="http://www.quanmin.tv/json/categories/list.json?09201104&device=869953020997409&v=2.1.3&screen=2&ch=wandoujia&sh=1812&sw=1080&uid=WTKDU16629006724&net=0&ver=4&os=1";
     public static final String LIVE_START="http://www.quanmin.tv/json/play/list";
-//    拼接 "_数字" 默认为空的
+//    拼接 "_数字" 默认为空
     public static final String LIVE_END=".json?09201103&device=869953020997409&v=2.1.3&screen=2&ch=wandoujia&sh=1812&sw=1080&uid=WTKDU16629006724&net=0&ver=4&os=1";
 
     //   推荐圆形分类图片内容接口  拼接方式: START+ "classification" + MIDDLE+ 当前时间 月 日 时 分 8位 +END
